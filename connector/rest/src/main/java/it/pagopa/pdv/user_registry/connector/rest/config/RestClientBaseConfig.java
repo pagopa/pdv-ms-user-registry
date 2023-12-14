d168d6084123ab1e99f95bc616f06b2501f77555
Benito Visone <benito.visone@pagopa.it>
2023-12-14 15:12:45 +0100

    fix: Rollback commit to deploy (#60)

--- connector/rest/src/main/java/it/pagopa/pdv/user_registry/connector/rest/config/RestClientBaseConfig.java
+++ connector/rest/src/main/java/it/pagopa/pdv/user_registry/connector/rest/config/RestClientBaseConfig.java
@@ -1,20 +1,14 @@
 package it.pagopa.pdv.user_registry.connector.rest.config;
 
-import com.amazonaws.xray.proxies.apache.http.HttpClientBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import feign.Client;
 import feign.RequestInterceptor;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
-import feign.httpclient.ApacheHttpClient;
+import feign.okhttp.OkHttpClient;
 import it.pagopa.pdv.user_registry.connector.rest.interceptor.QueryParamsPlusEncoderInterceptor;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.cloud.openfeign.support.FeignHttpClientProperties;
 import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
@@ -30,11 +24,6 @@ public class RestClientBaseConfig {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Value("${feign.client.config.default.connectTimeout}")
-    private int connectTimeout;
-
-    @Value("${feign.client.config.default.readTimeout}")
-    private int readTimeout;
 
     @Bean
     public RequestInterceptor queryParamsPlusEncoderInterceptor() {
@@ -58,36 +47,11 @@ public class RestClientBaseConfig {
 
         return new SpringEncoder(objectFactory);
     }
-    @Bean
-    public HttpClientBuilder xrayHttpClientBuilder() {
-
-        return HttpClientBuilder.create();
-    }
     @Configuration
     public class FeignConfiguration {
         @Bean
-        public Client client(HttpClientBuilder httpClientBuilder, FeignHttpClientProperties httpClientProperties) {
-
-            httpClientBuilder = httpClientBuilder != null ? httpClientBuilder : HttpClientBuilder.create();
-
-            final PoolingHttpClientConnectionManager poolingHttpClientConnectionManager =
-                    new PoolingHttpClientConnectionManager();
-            poolingHttpClientConnectionManager.setDefaultMaxPerRoute(1000);
-            poolingHttpClientConnectionManager.setMaxTotal(1000);
-
-            final RequestConfig defaultRequestConfig = RequestConfig.custom()
-                    .setConnectionRequestTimeout(connectTimeout)
-                    .setConnectTimeout(httpClientProperties.getConnectionTimeout())
-                    .setSocketTimeout(readTimeout)
-                    .setRedirectsEnabled(httpClientProperties.isFollowRedirects())
-                    .build();
-
-            return new ApacheHttpClient(
-                    httpClientBuilder
-                            .setConnectionManager(poolingHttpClientConnectionManager)
-                            .setDefaultRequestConfig(defaultRequestConfig)
-                            .build()
-            );
+        public OkHttpClient client() {
+            return new OkHttpClient();
         }
     }
 
