eb526f6e7421426db219d3e2717faee89e7562ce
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2024-08-05 14:06:04 +0200

    chore: edit to schemas for swagger config

--- web/src/main/java/it/pagopa/pdv/user_registry/web/config/SwaggerConfig.java
+++ web/src/main/java/it/pagopa/pdv/user_registry/web/config/SwaggerConfig.java
@@ -61,9 +61,9 @@ class SwaggerConfig {
                         .addSchemas("NameCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.name}" ))
                         .addSchemas("FamilyNameCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.familyName}" ))
                         .addSchemas("EmailCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.email}" ))
-                        .addSchemas("MobilePhoneCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.mobilePhone}" ))
-                        .addSchemas("TelephoneCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.telephone}" ))
-                        .addSchemas("BirthDateCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceDate.class, "${swagger.model.user.birthDate}" )));
+                        .addSchemas("BirthDateCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceDate.class, "${swagger.model.user.birthDate}" ))
+                        .addSchemas("MobilePhoneCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.workContact.mobilePhone}" ))
+                        .addSchemas("TelephoneCertifiableSchema", getSchemaWithDifferentDescription(CertifiableFieldResourceString.class, "${swagger.model.user.workContact.telephone}" )));
 
     }
 
